4f4bdd3b15e8bc0417c6c7bc31cf5a6a06ef37d2
Dominik Böhler <dominik.boehler@gmx.net>
2025-01-08 09:35:55 +0100

    some fixes using clippy

--- src/editor.rs
+++ src/editor.rs
@@ -1,4 +1,3 @@
-use std::io::{self, Read};
 use crossterm::event::{read, Event::Key, KeyCode::Char};
 use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
 
@@ -6,28 +5,33 @@ pub struct Editor {
 
 }
 
+impl Default for Editor {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl Editor {
 
-    pub fn default() -> Self {
+    #[must_use]
+    pub fn new() -> Self {
         Editor{}
     }
 
+    #[allow(clippy::panic)]
     pub fn run(&self){
         enable_raw_mode().unwrap();
         loop {
             match read() {
                 Ok(Key(event)) => {
-                    println!("{:?} \r", event);
-                    match (event.code) {
-                        Char(c) => {
-                            if c == 'q' {
-                                break;
-                            }
-                        },
-                        _ => (),
+                    println!("{event:?} \r");
+                    if let Char(c) = event.code {
+                        if c == 'q' {
+                            break;
+                        }
                     }
                 },
-                Err(err) => println!("Error: {}", err),
+                Err(err) => println!("Error: {err}"),
                 _ => ()
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+#![warn(clippy::all, clippy::pedantic)]
+
 use rim::editor::Editor;
 
 fn main() {
-    let editor = Editor::default();
+    let editor = Editor::new();
     editor.run();
 }
