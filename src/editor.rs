2e5bfd25a23b1978ae27dd338a7e0de5ca796073
Dominik Böhler <dominik.boehler@gmx.net>
2025-01-08 13:22:09 +0100

    refactoring into editor and terminal

--- src/editor.rs
+++ src/editor.rs
@@ -4,48 +4,33 @@ use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
 use crossterm::cursor::MoveTo;
 use std::io::stdout;
 
+use crate::terminal::Terminal;
+
 pub struct Editor {
     should_quit: bool,
 }
 
 impl Editor {
 
-    pub fn default() -> Self {
-        Editor { should_quit: false }
+    pub const fn default() -> Self {
+        Self { should_quit: false }
     }
 
     pub fn run(&mut self) {
-        Self::initialize().unwrap();
-        self.refresh_screen();
+        Terminal::initialize().unwrap();
         let result = self.repl();
-        Self::terminate().unwrap();
+        Terminal::terminate().unwrap();
         result.unwrap();
-    }
-
-    fn initialize() -> Result<(), std::io::Error> {
-        enable_raw_mode()?;
-        Self::clear_screen();
-        execute!(stdout(), MoveTo(0, 0))?;
-        Ok(())
-    }
-
-    fn terminate() -> Result<(), std::io::Error> {
-        disable_raw_mode()
-    }
-
-    fn clear_screen() -> Result<(), std::io::Error> {
-        let mut stdout = stdout();
-        execute!(stdout, Clear(ClearType::All))
     }
 
     fn repl(&mut self) -> Result<(), std::io::Error> {
         loop {
-            let event = read()?;
-            self.evaluate_event(&event);
             self.refresh_screen()?;
             if self.should_quit {
                 break;
             }
+            let event = read()?;
+            self.evaluate_event(&event);
         }
         Ok(())
     }
@@ -66,7 +51,7 @@ impl Editor {
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         if self.should_quit {
-            Self::clear_screen()?;
+            Terminal::clear_screen()?;
             print!("Goodbye.\r\n");
         } else {
             Self::draw_rows();
@@ -75,12 +60,12 @@ impl Editor {
         Ok(())
     }
 
-    fn draw_rows() {
-        if let Ok((_width, height)) = crossterm::terminal::size() {
-            for line in 0..=height {
-                execute!(stdout(), MoveTo(0, line));
-                print!("~");
-            }
+    fn draw_rows() -> Result<(), std::io::Error> {
+        let height = Terminal::size()?.1;
+        for line in 0..=height {
+            execute!(stdout(), MoveTo(0, line));
+            print!("~");
         }
+        Ok(())
     }
 }
