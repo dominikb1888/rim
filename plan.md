c32d31ae3dddd5340037a1acf32af43188a511b7
Dominik Böhler <dominik.boehler@gmx.net>
2025-01-16 11:37:16 +0100

    added error handling, some refactoring

---
+++ plan.md
@@ -0,0 +1,20 @@
+60min:
+- [ ] Improved Error Handling
+- [ ] VT100 Key Strokes and Abstractions
+- [ ] Code Cleaning (initialize, replace, terminate, eval_event, refresh_screen)
+
+30min:
+- [ ] Assignment: Tildes
+
+20min:
+- [ ] Better Drawing of Tildes (use of crossterm API) 
+
+20min:
+- [ ] Assignment: Hiding of Cursor on Startup
+
+20min:
+- [ ] Welcome Message
+- [ ] usize Problem
+
+30min:
+- [ ] Assignment: Caret Movement
--- src/editor.rs
+++ src/editor.rs
@@ -1,46 +1,54 @@
 use std::io::stdout;
 
 use crossterm::terminal::{Clear, ClearType, enable_raw_mode, disable_raw_mode};
-use crossterm::event::{read, Event::Key, KeyCode::Char};
+use crossterm::event::{read, KeyEvent, Event::Key, KeyModifiers, KeyCode::Char};
 use crossterm::execute;
 
-pub struct Editor { }
+pub struct Editor {
+    should_quit: bool,
+}
 
 
 impl Editor {
 
     pub fn default() -> Self {
-        Self { }
+        Self { should_quit: false }
     }
 
-    pub fn run(&self) {
-        enable_raw_mode().unwrap();
-        Self::clear_screen();
-        Self::repl();
+    pub fn run(&mut self) {
+        if let Err(err) = self.repl() {
+            panic!("{err:#?}");
+        }
+        print!("Goodbye.\r\n");
+    }
+
+    fn terminate(&self) -> Result<(), std::io::Error> {
+        disable_raw_mode()?;
+        Ok(())
     }
 
     fn clear_screen() -> Result<(), std::io::Error> {
         execute!(stdout(), Clear(ClearType::All))
     }
 
-    pub fn repl() {
+    pub fn repl(&mut self) -> Result<(), std::io::Error> {
+        Self::clear_screen()?;
+        enable_raw_mode()?;
         loop {
-            match read() {
-                Ok(Key(event)) => {
-                    match event.code {
-                        Char('q') => {
-                            disable_raw_mode().unwrap();
-                            break;
-                        }
-                        Char(c) => {
-                            println!("{c}");
-                        }
-                        _ => { todo!() }
+            if let Key(KeyEvent { code, modifiers, kind, state }) = read()? {
+                println!("Code: {code:?} Modifiers: {modifiers:?} Kind: {kind:?} State: {state:?} \r");
+                match code {
+                    Char('q') if modifiers == KeyModifiers::CONTROL => {
+                        self.should_quit = true;
                     }
+                    _ => (),
                 }
-                Err(err) => println!("Error: {err}"),
-                _ => todo!(),
+            }
+            if self.should_quit {
+                break;
             }
         }
+        disable_raw_mode()?;
+        Ok(())
     }
 }
