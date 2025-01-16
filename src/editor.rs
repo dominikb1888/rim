841e8bb309fc3f69d49cc89e49fc1a8f21ba2dcf
Dominik Böhler <dominik.boehler@gmx.net>
2025-01-16 13:10:05 +0100

    after session jan 16, with bugs in welcome message

--- src/editor.rs
+++ src/editor.rs
@@ -1,8 +1,12 @@
 use std::io::stdout;
 
-use crossterm::terminal::{Clear, ClearType, enable_raw_mode, disable_raw_mode};
+use crossterm::terminal::{Clear, ClearType, enable_raw_mode, disable_raw_mode, size};
 use crossterm::event::{read, KeyEvent, Event, Event::Key, KeyModifiers, KeyCode::Char};
+use crossterm::cursor::MoveTo;
 use crossterm::execute;
+
+const NAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 pub struct Editor {
     should_quit: bool,
@@ -24,7 +28,9 @@ impl Editor {
 
     fn initialize() -> Result<(), std::io::Error> {
         enable_raw_mode()?;
-        Self::clear_screen()
+        Self::clear_screen()?;
+        Self::draw_rows()?;
+        Self::draw_welcome_message()
     }
 
     fn terminate() -> Result<(), std::io::Error> {
@@ -67,6 +73,30 @@ impl Editor {
         if self.should_quit {
             Self::clear_screen()?;
             print!("Goodbye.\r\n");
+        }
+        Ok(())
+    }
+
+    fn draw_rows() -> Result<(), std::io::Error> {
+        let (height, width) = size()?;
+        execute!(stdout(), MoveTo(0,0));
+        for i in 0..=height {
+            print!("~");
+            execute!(stdout(), MoveTo(0, i));
+        }
+        execute!(stdout(), MoveTo(0,0));
+        Ok(())
+    }
+
+    fn draw_welcome_message() -> Result<(), std::io::Error> {
+        let (height, width) = size()?;
+        let message = format!("{NAME} - {VERSION}");
+        let y = height as usize / 3;
+        let x = (width as usize / 2) - message.len() / 2;
+        execute!(stdout(), MoveTo(x.try_into().unwrap(), y.try_into().unwrap()))?;
+        for (i, c) in message.chars().enumerate() {
+            print!("{c}");
+            execute!(stdout(), MoveTo((x + i).try_into().unwrap(),y.try_into().unwrap()))?;
         }
         Ok(())
     }
